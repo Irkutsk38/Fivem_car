a07a3e19d210e6c097c80a418bd42ca54fd6e65d
Irkutsk86 <alexsaynsk13@mail.ru>
2020-01-23 18:36:55 +0800

    rmodamgc63

---
+++ rmodamgc63/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+game 'gta5'
+ 
+files {
+    'carvariations.meta',
+    'dlctext.meta',
+    'handling.meta',
+    'vehicles.meta',
+	'carcols.meta',
+	'vehiclelayouts.meta',
+	'shop_vehicle.meta',
+	'rmodamgc63contentunlocks.meta'
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_LAYOUTS_FILE''vehiclelayouts.meta'
+data_file 'SHOP_VEHICLE_FILE''shop_vehicle.meta'
+data_file 'RMODAMGC63_CONTENT_UNLOCKS_FILE''rmodamgc63contentunlocks.meta'
+
+ 
+client_script 'vehicle_names.lua'
