5fb7417bd6bdf226147b9f10c9f3e45872d8b79b
Irkutsk86 <alexsaynsk13@mail.ru>
2020-01-25 22:04:36 +0800

    ELS-FiveM

---
+++ [els]/ELS-FiveM/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'ELS FiveM'
+
+version '1.7'
+
+client_script {
+	'vcf.lua',
+	'config.lua',
+	'client/util.lua',
+	'client/client.lua',
+	'client/_patternTypes/leds.lua',
+	'client/_patternTypes/traf.lua',
+	'client/_patternTypes/chp.lua',
+	'client/patterns.lua'
+}
+
+server_script {
+	'vcf.lua',
+	'config.lua',
+	'server/server.lua',
+	'server/xml.lua'
+}
+
+shared_script 'shared/*.lua'
